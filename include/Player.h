e7d7922a230f8ead501fed3de1ecce305f81269f
bogdanovich.dima@phystech.edu <bogdanovich.dima@phystech.edu>
2019-03-28 00:52:24 +0300

    Добавил классы игрока и фабрики юнитов

---
+++ include/Player.h
@@ -0,0 +1,19 @@
+//
+// Created by exoron on 28.03.19.
+//
+
+#ifndef STRATEGY_GAME_PLAYER_H
+#define STRATEGY_GAME_PLAYER_H
+
+#include <vector>
+
+#include <Unit.h>
+#include <UnitFactory.h>
+
+class Player {
+
+ private:
+  std::vector<Unit> units_;
+};
+
+#endif //STRATEGY_GAME_PLAYER_H
---
+++ include/UnitFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by exoron on 28.03.19.
+//
+
+#ifndef STRATEGY_GAME_UNITFACTORY_H
+#define STRATEGY_GAME_UNITFACTORY_H
+
+#include <Unit.h>
+
+class UnitFactory {
+
+};
+
+#endif //STRATEGY_GAME_UNITFACTORY_H
