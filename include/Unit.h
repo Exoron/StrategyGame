fc034636ffbec99bc93c0788ce6242d46741348c
bogdanovich.dima@phystech.edu <bogdanovich.dima@phystech.edu>
2019-03-28 00:37:43 +0300

    Минимальная рабочая структура

---
+++ include/Unit.h
@@ -0,0 +1,12 @@
+//
+// Created by exoron on 28.03.19.
+//
+
+#ifndef STRATEGY_GAME_UNIT_H
+#define STRATEGY_GAME_UNIT_H
+
+class Unit {
+
+};
+
+#endif //STRATEGY_GAME_UNIT_H
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by exoron on 27.03.19.
+//
+
+#include <iostream>
+
+#include <Unit.h>
+
+int main() {
+  std::cout << "Success" << std::endl;
+}
