13b7513645961fc6fd9f632142bd9048a372f910
bogdanovich.dima@phystech.edu <bogdanovich.dima@phystech.edu>
2019-03-29 21:58:05 +0300

    Добавил новую фракцию с одним юнитом и фабрикой

---
+++ include/alloys_factories/OreMine.h
@@ -0,0 +1,15 @@
+//
+// Created by exoron on 29.03.19.
+//
+
+#ifndef STRATEGY_GAME_OREMINE_H
+#define STRATEGY_GAME_OREMINE_H
+
+#include <UnitFactory.h>
+
+class OreMine : public UnitFactory {
+ public:
+  Unit* create_unit() const override;
+};
+
+#endif //STRATEGY_GAME_OREMINE_H
